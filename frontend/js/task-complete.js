c4cb9ef7772c269a3d96e6bd86652aee5e2a046d
Adedamola Omoniyi <damolaominiyi@gmail.com>
2025-11-08 16:27:19 +0000

    fixed the task complete page

--- frontend/js/task-complete.js
+++ frontend/js/task-complete.js
@@ -1,7 +1,24 @@
-/* TODO: 
-1) Make HTML more accessible and semantic for seo
-2) Use better class names and variables for css
-3) Implement js
-4) Add more interactibility and polish
-5)Implement light mode
-*/
+async function loadProgress() {
+
+const result = await apiFetch("/api/tasks/progress/", { method: "GET" });
+  if (!result.ok) {
+    console.error("Failed");
+    return "Unable to get task data";
+  }
+  const data = await result.json();
+  document.getElementById("tasks-completed").textContent = data.completed;
+  document.getElementById("tasks-remaining").textContent = data.total - data.completed;
+  return `${data.completed}/${data.total} Tasks Completed`;
+}
+
+
+loadProgress()
+
+const params = new URLSearchParams(window.location.search)
+const taskName = params.get("name")
+document.getElementById("completed-task-name").textContent = taskName;
+
+function addTask() {
+  window.location.href = "add-task.html";
+}
+document.getElementById("add-task-btn").addEventListener("click", addTask);
